d34ce2729c6c341a593b5da472efd54d8fd31d31
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2024-05-12 05:16:51 +0300

    making deploy file

---
+++ deploy.py
@@ -0,0 +1,71 @@
+from flask import Flask, request,jsonify
+import pandas as pd
+from flask_cors import CORS
+from os.path import join, dirname, realpath
+import json
+import glob
+import math
+import os
+import datetime
+from flask import request
+from PIL import Image
+from lpq import lpq
+import pickle
+import cv2
+import numpy as np
+from preprocessingunit import process_image,preprocess
+app = Flask(__name__)
+CORS(app)
+
+def display(img, frameName="OpenCV Image"):
+    
+    h, w = img.shape[0:2]
+    neww = 800
+    newh = int(neww*(h/w))
+    img = cv2.resize(img, (neww, newh))
+    cv2.imshow(frameName, img)
+    cv2.waitKey(0)
+# model=pickle.load("Logistic Regression.pkl")
+with open('Logistic Regression.pkl', 'rb') as file:
+    # Deserialize and load the object from the file
+    model = pickle.load(file)
+
+# app.config['PHOTO_PATH'] = join(dirname(realpath(__file__)), 'Photos')
+
+@app.route("/image_3", methods=["POST"])
+def imagePrediction():
+    file=request.files['image']
+    # img=Image.open(file.stream)
+    # img.save('uploaded_image.jpeg')
+    img=cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+    # img=Image.open(file.stream)
+    # img_path = join(app.config['PHOTO_PATH'], 'uploaded_image.jpeg')
+    # display(img)   
+    # print(img) 
+    # # Process the image
+    proceseed_img=preprocess(img, True)
+
+    # # # Load the preprocessed image (assuming it's saved)
+    # path="Processed-test/test/"+img
+    # image = cv2.imread(path)
+
+    # # # Extract LPQ features or other processing if needed
+    imageLPQ = lpq(proceseed_img,b=True)  # Replace with your specific feature extraction logic
+    # # # Predict the label using your model
+    label = model.predict(np.array([imageLPQ]))[0]
+    # return 
+    return jsonify({'label': str(label)})
+
+# check if the post request has the file part
+    # if 'file' not in request.files:
+    #         return "No file found"
+    # user_file = request.files['file']
+    # if user_file.filename == "":
+    #     return "file name not found"
+    # else:
+    #     path=os.path.join(os.getcwd()+'\\modules\\static\\'+user_file.filename)
+    #     user_file.save(path)
+
+app.run(host='localhost', port=5000)
+
+
--- preprocessingutils.py
+++ preprocessingutils.py
@@ -1,23 +1,18 @@
-# import cv2
-# import numpy as np
-from os.path import  join
+
+import numpy as np
+import cv2
+from skimage.feature import hog, local_binary_pattern
+from os import listdir, mkdir
+from os.path import isfile, join, dirname, exists
 from PIL import Image
+import os
+# from sklearn.svm import SVC
+from scipy.signal import convolve2d
+# from sklearn.externals import joblib
+# import joblib
+import matplotlib.pyplot as pyplot
 
-
-debug = True
-
-# #Display image
-def display(img, frameName="OpenCV Image"):
-    if not debug:
-        return
-    h, w = img.shape[0:2]
-    neww = 800
-    newh = int(neww*(h/w))
-    img = cv2.resize(img, (neww, newh))
-    cv2.imshow(frameName, img)
-    cv2.waitKey(0)
-
-#rotate the image with given theta value
+# rotate the image with given theta value
 def rotate(img, theta):
     rows, cols = img.shape[0], img.shape[1]
     image_center = (cols/2, rows/2)
@@ -33,21 +28,9 @@ def rotate(img, theta):
     M[0, 2] += bound_w/2 - image_center[0]
     M[1, 2] += bound_h/2 - image_center[1]
 
-    print(img[0][0])
     # rotate orignal image to show transformation
-    print(img[0][0])
-
     rotated = cv2.warpAffine(img,M,(bound_w,bound_h),borderValue=(int(img[0][0]),int(img[0][0]),int(img[0][0])))
     return rotated
-
-
-# def slope(x1, y1, x2, y2):
-#     if x1 == x2:
-#         return 0
-#     slope = (y2-y1)/(x2-x1)
-#     theta = np.rad2deg(np.arctan(slope))
-#     return theta
-
 def round_to_angle_multiples(number):
 
     # Define the possible angles
@@ -63,81 +46,6 @@ def round_to_angle_multiples(number):
     return angles[min_index]
 
 
-
-
-
-# def main(filePath):
-#     img = cv2.imread(filePath)
-#     textImg = img.copy()
-
-#     small = cv2.cvtColor(textImg, cv2.COLOR_BGR2GRAY)
-
-#     #find the gradient map
-#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-#     grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
-
-#     display(grad)
-
-#     #Binarize the gradient image
-#     _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-#     display(bw)
-
-#     #connect horizontally oriented regions
-#     #kernal value (9,1) can be changed to improved the text detection
-#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
-#     connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
-#     display(connected)
-
-#     # using RETR_EXTERNAL instead of RETR_CCOMP
-#     # _ , contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-#     contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) #opencv >= 4.0
-
-
-#     mask = np.zeros(bw.shape, dtype=np.uint8)
-#     #display(mask)
-#     #cumulative theta value
-#     cummTheta = 0
-#     #number of detected text regions
-#     ct = 0
-#     for idx in range(len(contours)):
-#         x, y, w, h = cv2.boundingRect(contours[idx])
-#         mask[y:y+h, x:x+w] = 0
-#         #fill the contour
-#         cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-#         #display(mask)
-#         #ratio of non-zero pixels in the filled region
-#         r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
-
-#         #assume at least 45% of the area is filled if it contains text
-#         if r > 0.45 and w > 8 and h > 8:
-#             #cv2.rectangle(textImg, (x1, y), (x+w-1, y+h-1), (0, 255, 0), 2)
-
-#             rect = cv2.minAreaRect(contours[idx])
-#             box = cv2.boxPoints(rect)
-#             box = np.int0(box)
-#             cv2.drawContours(textImg,[box],0,(0,0,255),2)
-
-#             #we can filter theta as outlier based on other theta values
-#             #this will help in excluding the rare text region with different orientation from ususla value 
-#             theta = slope(box[1][0], box[1][1], box[2][0], box[2][1])
-#             cummTheta += theta
-#             ct +=1 
-#             print("Theta", theta)
-            
-#     #find the average of all cumulative theta value
-#     orientation = cummTheta/ct
-#     orientation=round_to_angle_multiples(orientation)
-#     print(orientation)
-#     print("Image orientation in degress: ", orientation)
-#     finalImage = rotate(img, -orientation)
-#     display(textImg, "Detectd Text minimum bounding box")
-#     display(finalImage, "Deskewed Image")
-
-# if __name__ == "__main__":
-#     filePath = '1.jpeg'
-#     main(filePath)
-import cv2
-import numpy as np
 def rotate_img(edgeimage,originalimage):
     # Apply Hough Lines Transform
     lines = cv2.HoughLinesP(edgeimage, rho=1, theta=np.pi/180, threshold=100, minLineLength=50, maxLineGap=10)
@@ -157,7 +65,6 @@ def rotate_img(edgeimage,originalimage):
 
         if longest_line is not None:
             x1, y1, x2, y2 = longest_line[0]
-            print(x1,y1,x2,y2)
             if x2 - x1 != 0:  # Avoid division by zero
                  orientation = np.arctan((y2 - y1) / (x2 - x1))*180  # Radians
             else:
@@ -175,14 +82,19 @@ def rotate_img(edgeimage,originalimage):
         #     display(img,"box")
 
     return finalImage
-    # print(orientation)
 
-def preprocess(image_path):
+
+
+def preprocess(image_path,b=False):
     # Read the image
-    originalimg = cv2.imread(image_path)
+    if not b :
+        img = cv2.imread(image_path)
+    else:
+        img = image_path
+
 
     # Salt and pepper noise
-    img=cv2.medianBlur(originalimg,5)
+    img=cv2.medianBlur(img,5)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # display(img)
 
@@ -192,131 +104,11 @@ def preprocess(image_path):
 
     rotated_img=rotate_img(edges,img)
     # display(img)
-
     if (img[0][0]>200):
         _, img = cv2.threshold(rotated_img, 50.0, 255.0, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+        print("white")
     else:
         _, img = cv2.threshold(rotated_img, 50.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-
-    # binary_img=rotated_img
-    # contours, hierarchy = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(originalimg, contours, -1, (0, 255, 0), 2)  # Green color, thickness 2
-    # display(originalimg)
-    # binary_img=img
-
-    # img=rotated_img
-    # display(rotated_img)
+        print("black")
     # display(img)
-
-    #Binarize the gradient image
-    # display(bw)
-    # display(img)
-    
-    #--morphological operations--
-    #find the gradient map
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-    # grad = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
-
-    # display(img)
-    # display(originalimg)
-
     return img
-    
-
-
-    # Display the image with the longest line (or nothing if no lines)
-
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-  
-
-# # Example usage
-# image_path = '11.jpeg'
-# img=preprocess(image_path)
-##########OCR
-# import cv2
-# import pytesseract
-
-# # Load the image
-# img = cv2.imread('22.jpeg')
-
-# # Convert the image to grayscale
-# # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# # Use pytesseract to perform OCR and get bounding box coordinates
-# detections = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
-
-# print(detections)
-# # Iterate over each detection
-# prev=detections['top'][0]
-# for i, text in enumerate(detections['text']):
-#     # Skip empty detections
-#     if text.strip() == '':
-#         continue
-    
-#     if detections['top'][i] -  
-#     # Extract bounding box coordinates
-#     x, y, w, h = detections['left'][i], detections['top'][i], detections['width'][i], detections['height'][i]
-    
-#     # Extract the line image
-#     line_image = img[y:y+h, x:x+w]
-    
-#     # Save the line image
-#     cv2.imwrite(f'line_{i+1}.jpg', line_image)
-
-
-def extractImageLines(img,img_name,output_dir,img_path):
-    
-    #cv2.RETR_EXTERNAL retrieves only the outermost contours of connected regions.
-    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # Sorting contours by their start position in y just to be processed from top to bottom
-    contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[1])
-
-    cur_line=[]
-    all_lines_found=[]
-    prev_y=-1
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        #compare aspect ratio w/h and area to filter contours
-        aspect_ratio = w / h
-        # aspect_ratio = w / float(h)
-
-        area=cv2.contourArea(contour)
-        if aspect_ratio <8 and aspect_ratio>0.3 and area >90:
-        # if 0.1 < aspect_ratio < 10 and cv2.contourArea(contour) > 100:
-            # print("here")
-            #if valid contour check if it belongs to previous line or it is the first contotur tot work on
-            if prev_y == -1 or abs(prev_y - y) < 17:
-            # if  len(all_lines_found)==0 or abs(prev_y -y) <20:
-                cur_line.append(contour)
-            else:
-                all_lines_found.append(cur_line)
-                cur_line=[]
-                cur_line.append(contour)
-            prev_y=y
-
-    # For the last line
-    if cur_line:
-        all_lines_found.append(cur_line)
-    # print(all_lines_found)
-    #then we have 2d array where for each line we have array of contours
-    #we want to find bounding box for each line
-    index=0
-    for line in all_lines_found:
-        # print(index)
-        #concatenates all contours into single one
-        line_contours = np.concatenate(line)
-        x, y, w, h = cv2.boundingRect(line_contours)
-        new_img = img[y:y+h, x:x+w]
-        name=img_name+'_'+str(index+1)+'.jpeg'
-        image = Image.fromarray(new_img)  # Convert to PIL Image object
-        new_path = join(output_dir, name)
-    
-        # new_path = join(output_dir, name)
-        image.save(new_path)
-        # cv2.imwrite(f'{name}.jpeg', new_img)
-        index=index+1
-
-# extractImageLines(img,'11')
-# cv2.drawContours(img, lines, -1, (0, 255, 0), 2)  # Green color, thickness 2
